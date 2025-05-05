b6304f6df67e47a4c49feb2a37a9ef569f71bec6
dogopequi <diogo.l.manteu3012@gmail.com>
2025-05-05 21:28:53 +0100

    saving before pulling

--- src/main.c
+++ src/main.c
@@ -29,16 +29,16 @@ int main(void)
     load_texture();
     create_world();
 
-    Shader shader = LoadShader(0, TextFormat("resources/shaders/swirl.fs", GLSL_VERSION));
+    /* Shader shader = LoadShader(0, TextFormat("resources/shaders/swirl.fs", GLSL_VERSION));
 
     // Get variable (uniform) location on the shader to connect with the program
     // NOTE: If uniform variable could not be found in the shader, function returns -1
     int swirlCenterLoc = GetShaderLocation(shader, "center");
 
     float swirlCenter[2] = { (float)screenWidth/2, (float)screenHeight/2 };
-
+ */
     // Create a RenderTexture2D to be used for render to texture
-    RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
+    //RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
     while (!WindowShouldClose())
     {
         UpdateCameraPro(&camera,
@@ -58,7 +58,7 @@ int main(void)
             GetMouseWheelMove()*2.0f);  
         Vector2 mousePosition = GetMousePosition();
 
-        swirlCenter[0] = mousePosition.x;
+       /*  swirlCenter[0] = mousePosition.x;
         swirlCenter[1] = screenHeight - mousePosition.y;
 
         SetShaderValue(shader, swirlCenterLoc, swirlCenter, SHADER_UNIFORM_VEC2);   
@@ -75,8 +75,14 @@ int main(void)
         DrawTextureRec(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, (float)-target.texture.height }, (Vector2){ 0, 0 }, WHITE);
         EndShaderMode();
         DrawFPS(10, 10);
+        EndDrawing(); */
+        BeginDrawing();
+        ClearBackground(RAYWHITE); 
+        BeginMode3D(camera);
+        draw_world(camera);
+        EndMode3D();
+        DrawFPS(10, 10);
         EndDrawing();
-
     }
     CloseWindow(); 
     CloseAudioDevice();
